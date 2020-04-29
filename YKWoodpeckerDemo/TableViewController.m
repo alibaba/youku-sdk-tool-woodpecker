ac94bd325f50acb5049bb9f48ab4b9d7243fa48f
Zim <zim.dzm@alibaba-inc.com>
2020-04-29 20:44:52 +0800

    Modify demo

--- YKWoodpeckerDemo/TableViewController.m
+++ YKWoodpeckerDemo/TableViewController.m
@@ -51,8 +51,8 @@ static NSString *_readme = nil;
         self.selectionStyle = UITableViewCellSelectionStyleNone;
         
         _label = [[UILabel alloc] init];
-        _label.font = [UIFont systemFontOfSize:15];
         _label.textColor = [UIColor colorWithRed:(arc4random() % 100 / 200.) green:(arc4random() % 100 / 200.) blue:(arc4random() % 100 / 200.) alpha:1];
+        _label.font = [UIFont systemFontOfSize:15];
         _label.numberOfLines = 0;
         [self addSubview:_label];
         
