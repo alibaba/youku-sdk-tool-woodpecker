7c91426e477c0d95a9478da43a5c6a0d2ca4b63e
Zim <zim.dzm@alibaba-inc.com>
2020-06-08 11:11:35 +0800

    Modify header import

--- YKWoodpecker/Plugins/UIComparePlugin/Panel/YKWUIComparePanel.h
+++ YKWoodpecker/Plugins/UIComparePlugin/Panel/YKWUIComparePanel.h
@@ -27,7 +27,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-#import <YKWoodpecker/YKWoodpecker.h>
+#import "YKWoodpecker.h"
 
 @interface YKWUIComparePanel : YKWFollowView
 
