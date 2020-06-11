9f58e51f4b4c91719f42b8b3a2b4d4244b829f18
Zim <zim.dzm@alibaba-inc.com>
2020-06-11 16:46:40 +0800

    Modify header file

--- YKWoodpecker/Category/YKWoodpeckerCommonHeaders.h
+++ YKWoodpecker/Category/YKWoodpeckerCommonHeaders.h
@@ -1,12 +1,30 @@
 //
 //  YKWoodpeckerCommonHeaders.h
-//  Pods
+//  YKWoodpecker
 //
-//  Created by Zim on 2020/6/8.
+//  Created by Zim on 2020/6/11.
 //
-
-#ifndef YKWoodpeckerCommonHeaders_h
-#define YKWoodpeckerCommonHeaders_h
+//  MIT License
+//
+//  Copyright (c) 2019 Alibaba
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 #import "UIView+YKWoodpeckerAdditions.h"
 #import "NSObject+YKWoodpeckerRuntime.h"
@@ -22,4 +40,3 @@
 #define YKWHighlightColor [UIColor ykw_colorWithHexString:@"ff00e6"]
 #define YKWINFOCOLOR [UIColor ykw_colorWithHexString:@"999999"]
 
-#endif /* YKWoodpeckerCommonHeaders_h */
